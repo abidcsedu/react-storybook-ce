b8d33ee530123f57f60baa58f8cdf7e2962dcc45
Md. Abid Raihan <abidcsedu@gmail.com>
2023-11-26 18:29:24 +0600

    sort stories by name

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,20 @@
 /** @type { import('@storybook/react').Preview } */
 const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		actions: { argTypesRegex: "^on[A-Z].*" },
+		options: {
+			storySort: (a, b) =>
+				a.id === b.id
+					? 0
+					: a.id.localeCompare(b.id, undefined, { numeric: true }),
+		},
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 };
 
 export default preview;
